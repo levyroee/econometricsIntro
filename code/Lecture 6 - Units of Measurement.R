2ab403d62a86818eabfa9c66c2d3329a847f08ef
Ro'ee Levy <43345240+levyroee@users.noreply.github.com>
2025-11-24 16:09:05 +0200

    lectures 5-6

--- code/Lecture 6 - Units of Measurement.R
+++ code/Lecture 6 - Units of Measurement.R
@@ -1,25 +1,52 @@
+library(ggplot2)
 library(dplyr)
+library(modelr) # modelr includes height
 
 rm(list=ls())
+
+heights <- heights %>% mutate(monthlyIncome = income / 12)
 
 men = heights %>% filter(sex=="male")
 women = heights %>% filter(sex=="female")
 
-heightIncomeMen = lm(data = men, formula = income ~ height)
+heightIncomeMen = lm(data = men, formula = monthlyIncome ~ height)
 summary(heightIncomeMen)
 
-heightIncomeWomen = lm(data = women, formula = income ~ height)
+heightIncomeWomen = lm(data = women, formula = monthlyIncome ~ height)
 summary(heightIncomeWomen)
 
-library(ggplot2)
+
 ggplot(women %>% sample_n(500), aes(x=height, y=income)) +
   geom_point() +
   theme_classic()
 
+# Manually convert coefficents
+inchToCm = 2.54
+beta_1 <- heightIncomeMen$coefficients[["height"]]
+beta_1 / inchToCm 
+
+usdToNis = 3.3
+beta_0 <- heightIncomeMen$coefficients[["(Intercept)"]]
+beta_0 * usdToNis 
+beta_1 / inchToCm  * usdToNis 
+
+beta_0 * usdToNis + (beta_1 / inchToCm  * usdToNis ) * 174
+
+
 
 men <- men %>% mutate(heightCM = height * 2.54, 
-                      incomeNIS = income / 12 * 3.8)
+                      incomeNIS = monthlyIncome * 3.3)
 
-summary(lm(data = men, formula = incomeNIS ~ heightCM))
+model = lm(data = men, formula = incomeNIS ~ heightCM)
+summary(model)
+predict(model, data.frame(heightCM=174))
 
 
+### Standard error
+summary(lm(data = men, formula = monthlyIncome ~ heightCM))
+33/2.54
+215-33.196
+215+33.196
+
+85-13.196
+85+13.196
